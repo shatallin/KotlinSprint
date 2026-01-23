5a9954018e41dc72ae9d411ee3b8b3ea4cf35ee8
shatalin <kshatallin@gmai.com>
2026-01-23 18:51:04 +0700

    KS-task-1-3

---
+++ utils/src/main/kotlin/lesson_2/lesson-1-3.kt
@@ -0,0 +1,21 @@
+package org.example.utils.lesson_2
+
+fun main () {
+
+  val year : Int = 1961   // год полета
+  var hour : Int = 9 // время взлета (часы)
+  var minute : Int = 7 // время взлета (минуты)
+  val stringValue : String = ":" //Символ
+
+    println(year)
+    println(hour)
+    println(minute)
+
+  hour = 10
+  minute = 55
+
+  print(hour)
+  print(stringValue)
+  print(minute)
+
+}
