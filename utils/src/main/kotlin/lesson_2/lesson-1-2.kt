7d064cc1ad2aafe734d3affb07be69670b1a1cf7
shatalin <kshatallin@gmai.com>
2026-01-23 18:03:53 +0700

    KS-task-1-2

---
+++ utils/src/main/kotlin/lesson_2/lesson-1-2.kt
@@ -0,0 +1,21 @@
+package org.example.utils.lesson_2
+
+fun main () {
+
+    val numberOfOrders: Int = 75
+    val thanksForThePurchase: String = ("Спасибо, что выбрали нас")
+
+    println(numberOfOrders)
+    println(thanksForThePurchase)
+
+
+    var numberOfEmployees : Int = 2000
+    println(numberOfEmployees)
+    val numberOfEmployeesText: String = ("Колличесво сотрудников в магазине:")
+    numberOfEmployees = 1999
+
+    println(numberOfEmployeesText)
+    println(numberOfEmployees)
+
+
+}
