3f4bd528a2dd04a2392beabcde3c3afe517ac518
shatalin <kshatallin@gmai.com>
2026-01-23 17:30:49 +0700

    KS-task-1-1

---
+++ utils/src/main/kotlin/lesson_2/lesson-1-1.kt
@@ -0,0 +1,10 @@
+package org.example.utils.lesson_2
+
+fun main () {
+
+    val numberOfOrders: Int = 75
+    val thanksForThePurchase: String = ("Спасибо, что выбрали нас")
+
+    println(numberOfOrders)
+    println(thanksForThePurchase)
+}
