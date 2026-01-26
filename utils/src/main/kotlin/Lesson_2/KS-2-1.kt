bdb42080deb094f1e456c5a0ca28a3a744d6da90
shatalin <kshatallin@gmai.com>
2026-01-26 20:12:49 +0700

    KS-task-2-1

---
+++ utils/src/main/kotlin/Lesson_2/KS-2-1.kt
@@ -0,0 +1,14 @@
+package org.example.utils.Lesson_2
+
+fun main () {
+
+    val a : Float = 3.0f
+    val b : Float = 4.0f
+    val c : Float = 3.0f
+    val d : Float = 5.0f
+
+    val averageScore : Float = (a+b+c+d)/4
+
+    println(averageScore)
+
+}
