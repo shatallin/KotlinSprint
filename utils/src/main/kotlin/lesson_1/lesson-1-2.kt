70b0917269ded6745fe6ca5cda89d25987799943
shatalin <kshatallin@gmai.com>
2026-01-26 17:50:58 +0700

    KS-task-1-2

---
+++ utils/src/main/kotlin/lesson_1/lesson-1-2.kt
@@ -0,0 +1,19 @@
+package org.example.utils.lesson_1
+
+fun main () {
+
+    val numberOfOrders: Int = 75
+    val thanksForThePurchase: String = ("Спасибо, что выбрали нас")
+
+    // выводим переменные из задачи 1
+    println(numberOfOrders)
+    println(thanksForThePurchase)
+
+    // количество работников
+    var numberOfEmployees: Int = 2000
+    println(numberOfEmployees)
+
+    // один работник уволился
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
