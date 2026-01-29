9a164e72ed699c52471d12456154ccbe73607596
shatalin <kshatallin@gmai.com>
2026-01-29 15:43:48 +0700

    KS-1-3 изучаем переменные

---
+++ utils/src/main/kotlin/lesson_1/KS-1-3.kt
@@ -0,0 +1,22 @@
+package org.example.utils.lesson_1
+
+fun main () {
+
+    val year : Int = 1961
+    var hour : Int = 9
+    var minute : Int = 7
+
+    val stringValue : String = ":"
+
+    println(year)
+    println("0"+hour)
+    println("0"+minute)
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(stringValue)
+    print(minute)
+
+}
